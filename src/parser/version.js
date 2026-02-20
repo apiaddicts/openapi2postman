2fa418ddb0a37515347747f1b0843299f767280e
Melsy Huamani <melsyhuamanivargas@gmail.com>
2026-02-20 09:20:24 -0500

    feat: update 3.1 versions range

--- src/parser/version.js
+++ src/parser/version.js
@@ -7,7 +7,7 @@ module.exports = function() {
   return function get(){
     const versions = {
       swagger : ['2.0', '2.0.0'],
-      openapi : ['3.0', '3.0.0', '3.0.1','3.0.2' , '3.0.3']
+      openapi : ['3.0', '3.0.0', '3.0.1','3.0.2', '3.0.3', '3.1.0', '3.1.1', '3.1.2']
     }
 
     if (versions.swagger.includes(global.definition.swagger)) {
