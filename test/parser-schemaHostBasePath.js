339642122ebccd805abd06b9d76b4341a5695679
Melsy Huamani <melsyhuamanivargas@gmail.com>
2026-05-07 19:17:33 -0500

    fix: duplicated code and sonar issues

--- test/parser-schemaHostBasePath.js
+++ test/parser-schemaHostBasePath.js
@@ -2,37 +2,25 @@
 
 const assert = require('node:assert');
 
+const openapi3Cases = [
+  { version: '3.0', seed: '../seeds/parserInitialGoodOpenApi3.json', host: 'https://petstore.swagger.io',  basePath: '/v1' },
+  { version: '3.1', seed: '../seeds/parserInitialGoodOpenApi3.1.json', host: 'https://petstore.swagger.io', basePath: '/v1' },
+  { version: '3.2', seed: '../seeds/parserInitialGoodOpenApi3.2.json', host: 'https://petstore.swagger.io', basePath: '/v1' },
+];
+
 describe('parser-schemaHostBasePath', () => {
 
   it('good swagger2', () => {
-
     globalThis.definition = require('../seeds/parserSchemaHostBasePathInitialGood.json');
-
     const schemaHostBasePath = require('../src/parser/swagger2/schemaHostBasePath.js')();
     assert.deepStrictEqual(schemaHostBasePath, { host: 'https://myserver.com', basePath: '/first/second' });
-  })
+  });
 
-  it('good openapi3.0', () => {
-
-    globalThis.definition = require('../seeds/parserInitialGoodOpenApi3.json');
-
-    const schemaHostBasePath = require('../src/parser/openapi3/schemaHostBasePath.js')();
-    assert.deepStrictEqual(schemaHostBasePath, { host: 'http://petstore.swagger.io', basePath: '/v1' });
-  })
-
-  it('good openapi3.1', () => {
-
-    globalThis.definition = require('../seeds/parserInitialGoodOpenApi3.1.json');
-
-    const schemaHostBasePath = require('../src/parser/openapi3/schemaHostBasePath.js')();
-    assert.deepStrictEqual(schemaHostBasePath, { host: 'https://petstore.swagger.io', basePath: '/v1' });
-  })
-
-  it('good openapi3.2', () => {
-
-    globalThis.definition = require('../seeds/parserInitialGoodOpenApi3.2.json');
-
-    const schemaHostBasePath = require('../src/parser/openapi3/schemaHostBasePath.js')();
-    assert.deepStrictEqual(schemaHostBasePath, { host: 'https://petstore.swagger.io', basePath: '/v1' });
-  })
+  openapi3Cases.forEach(({ version, seed, host, basePath }) => {
+    it(`good openapi${version}`, () => {
+      globalThis.definition = require(seed);
+      const schemaHostBasePath = require('../src/parser/openapi3/schemaHostBasePath.js')();
+      assert.deepStrictEqual(schemaHostBasePath, { host, basePath });
+    });
+  });
 });
